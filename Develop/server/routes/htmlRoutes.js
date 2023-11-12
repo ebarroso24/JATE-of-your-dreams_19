be127aba165db150442554c7e796a9410babd721
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-12 10:34:21 -0500

    fixing bugs in the code

--- Develop/server/routes/htmlRoutes.js
+++ Develop/server/routes/htmlRoutes.js
@@ -1,6 +1,13 @@
+// const path = require('path');
+
+// module.exports = (app) =>
+//   app.get('/', (req, res) =>
+//     res.sendFile(path.join(__dirname, '../client/index.html'))
+//   );
+
 const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../../client/index.html'))
   );
