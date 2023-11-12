8aafb7231ad68f42e50ce89bdabe981c29808a1b
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-11 22:16:59 -0500

    solved TODO's in the src/js folder

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  ({ request }) =>['style', 'script', 'image'].includes(request.destination),
+
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+
+        maxAgeSeconds: 7 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  await store.put({ content });
+
+
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  return store.getAll();
+};
+
 
 initdb();
